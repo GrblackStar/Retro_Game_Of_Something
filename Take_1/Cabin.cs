4320f1cc00d99289b08e0968c03fbcd26e7f78d7
HHristova <hristiana_h@yahoo.com>
2023-10-22 23:40:59 +0300

    Cabin model added

---
+++ Take_1/Cabin.cs
@@ -0,0 +1,26 @@
+﻿using Emotion.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Take_1
+{
+    public class Cabin : BaseTakeOneObject
+    {
+        public Cabin()
+        {
+            this.ShouldApplyToGrid = true;
+            EntityPath = "Custom/cabin.fbx";
+            Size3D = new System.Numerics.Vector3(50, 50, 50);
+        }
+
+        public override void Init()
+        {
+            base.Init();
+            Entity.LocalTransform = System.Numerics.Matrix4x4.Identity;
+            Entity.BackFaceCulling = false;
+        }
+    }
+}
